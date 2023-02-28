77b0cc028bbafcdd946ce2d2ec717e9d739b2abd
Srivatsan S <114812178+Srivatsan8055@users.noreply.github.com>
2023-02-28 19:15:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1; //random number 1 to 6
 
 var randomDiceImage1 = "dice" + randomNumber1 + ".png";
 
-var randomImageSource = "Images/" + randomDiceImage1;
+var randomImageSource = randomDiceImage1;
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -13,7 +13,7 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage2 = "dice" + randomNumber2 + ".png";
 
-var randomImageSource2 = "images/" + randomDiceImage2;
+var randomImageSource2 = randomDiceImage2;
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
